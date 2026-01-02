ca96f7bd02263e44d77e58cacb815b95f95772f5
wordforword <252376635+wordforword@users.noreply.github.com>
2026-01-01 20:38:49 -0600

    Add Side component

---
+++ src/components/Side.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+function handle(c: ClipboardEvent) {
+  if (!(c.target instanceof Element)) {
+    return;
+  }
+  const article = c.target.parentElement?.querySelector(`article`);
+  if (article) {
+    article.textContent = c.clipboardData?.getData(`text/plain`) ?? null;
+  }
+  article?.classList.remove(`invisible`);
+  c.target.classList.add(`invisible`);
+}
+</script>
+
+<template>
+  <div>
+    <article class="invisible"></article>
+    <textarea @paste="handle"></textarea>
+  </div>
+</template>
+
+<style scoped>
+.invisible {
+  display: none;
+}
+
+div {
+  flex: 1;
+}
+
+article, textarea {
+  width: 100%;
+  height: 100%;
+  font-family: monospace;
+}
+
+.read-the-docs {
+  color: #888;
+}
+</style>
