d4a799af63c09e5dd65b75c98f734f6a177e3f64
wordforword <252376635+wordforword@users.noreply.github.com>
2026-01-02 02:28:15 -0600

    Underline selection

--- src/components/Paragraph.vue
+++ src/components/Paragraph.vue
@@ -1,11 +1,31 @@
 <template>
-<p>
+<p @mouseup="handle">
   <slot></slot>
 </p>
 </template>
 
 <script setup lang="ts">
-
+function handle() {
+  const sel = window.getSelection();
+  if (!sel) {
+    return;
+  }
+  if (sel.isCollapsed) {
+    sel.modify(`move`, `forward`, `character`);
+    sel.modify(`move`, `backward`, `word`);
+    sel.modify(`extend`, `forward`, `word`);
+  }
+  while (/\s$/.test(sel.toString())) {
+    sel.modify(`extend`, `backward`, `character`);
+  }
+  const s = sel.toString();
+  const span = document.createElement(`span`);
+  span.textContent = s;
+  const range = sel.getRangeAt(0);
+  range.deleteContents();
+  range.insertNode(range.createContextualFragment(`<span style='text-decoration:underline;'>${s}</span>`));
+  sel.empty();
+}
 </script>
 
 <style scoped>
